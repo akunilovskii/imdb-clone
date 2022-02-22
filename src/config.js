bfbd890cf63096fb7cb42cc5e533b4edb63618b5
Aleksandr <17989652+akunilovskii@users.noreply.github.com>
2022-02-21 22:56:37 -0500

    Replaced data array with data object. Added poster images.

---
+++ src/config.js
@@ -0,0 +1,13 @@
+//////////////////////////////////////////////////////////////////////////
+// VARIABLES & STATE /////////////////////////////////////////////////////
+
+export let direction = "auto";
+export let counter = 0;
+export const slideShowSpeed = 5;
+export const slidesContainer = document.querySelector(".container__trailer-slider");
+export const trailersSet = [
+  "trailer1.jpg",
+  "trailer2.jpg",
+  "trailer3.jpg",
+  "trailer4.jpg",
+];
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import * as config from "./config.js";
+
 //////////////////////////////////////////////////////////////////////////
 // VARIABLES & STATE /////////////////////////////////////////////////////
 
@@ -7,21 +9,50 @@ let direction = "auto";
 let counter = 0;
 const slideShowSpeed = 5;
 const slidesContainer = document.querySelector(".container__trailer-slider");
-const trailersSet = [
-  "trailer1.jpg",
-  "trailer2.jpg",
-  "trailer3.jpg",
-  "trailer4.jpg",
-];
+
+const moviesSet = {
+  id1: {
+    title: "The Endgame",
+    image: "1-trailer.jpg",
+    poster: "1-poster.jpg",
+    duration: "2:01",
+    subtitle: "Watch the New Trailer",
+  },
+  id2: {
+    title: "Doctor Strange in the Multiverse of Madness",
+    image: "2-trailer.jpg",
+    poster: "2-poster.jpg",
+    duration: "2:17",
+    subtitle: "Watch the New Trailer",
+  },
+  id3: {
+    title: "The Lord of the Rings: The Rings of Power",
+    image: "3-trailer.jpg",
+    poster: "3-poster.jpg",
+    duration: "1:01",
+    subtitle: "Watch the New Trailer",
+  },
+  id4: {
+    title: "Fantastic Beasts: The Secrets of Dumbledore",
+    image: "4-trailer.jpg",
+    poster: "4-poster.jpg",
+    duration: "2:44",
+    subtitle: "Watch the New Trailer",
+  },
+};
+const moveisSetKeysArray = Object.keys(moviesSet);
 
 //////////////////////////////////////////////////////////////////////////
 // DOM INIT  /////////////////////////////////////////////////////////////
 
-const setSlides = trailersSet
-  .map((el, i) => {
+const setSlides = moveisSetKeysArray
+  .map((movie, i) => {
+    console.log(moviesSet[movie]["image"]);
     return `<div class="container__trailer-slide" style="transform: translateX(${
       100 * (i - 1)
-    }%)" ><img src="img/${el}" alt="Trailer image" class="trailer__img" id="trailer-img-${i}"></div>`;
+    }%)" ><img src="img/${
+      moviesSet[movie]["image"]
+    }" alt="Trailer image" class="trailer__img" id="trailer-img-${i}"></div>`;
   })
   .join("");
 
@@ -45,7 +76,7 @@ document.querySelectorAll(".trailer__button").forEach((btn) =>
 // INTERVAL CONTROLLER ///////////////////////////////////////////////////
 
 const onLoadSlideShow = setInterval(() => {
-  if (direction === "auto" && counter < trailersSet.length) {
+  if (direction === "auto" && counter < moveisSetKeysArray.length) {
     slideChangeHandler();
     counter++;
   } else {
@@ -54,7 +85,7 @@ const onLoadSlideShow = setInterval(() => {
 }, slideShowSpeed * 1000);
 
 function stopSlideShow() {
-  clearInterval(initialSlideShow);
+  clearInterval(onLoadSlideShow);
 }
 
 //////////////////////////////////////////////////////////////////////////
